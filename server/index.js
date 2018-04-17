0ba1b4e567b7ff764a2a57d82d503988d392155d
Georgina Hoagland <georgina.hoagland@gmail.com>
2018-04-17 13:26:15 -0400

    initialized server and client files

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 8080;
+module.exports = app;
+
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// sends index.html
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+//now serving on port (default 8080)
+app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  mode: 'development',
+  entry: './client/index.jsx',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js',
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.svg$|\.ttf?|\.woff$|\.woff2|\.eof|\.eot/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.webpack.js', '.web.js', '.js', '.json', '.jsx'],
+  },
+};
