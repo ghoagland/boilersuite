aa6a85180965c68188db226cf462fd95f05712ab
Georgina Hoagland <georgina.hoagland@gmail.com>
2018-04-18 14:51:33 -0400

    added eslint config

--- server/index.js
+++ server/index.js
@@ -2,34 +2,36 @@ const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 module.exports = app;
 
-//logging
-app.use(morgan('dev'))
+// logging
+app.use(morgan('dev'));
 
-//parsing
+// parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//api routes
+// api routes
 app.use('/api', require('./api'));
 
 // static file-serving middleware
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 // sends index.html
-app.get('*', function (req, res, next) {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
 // error handling endware
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   console.error(err);
   console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error');
 });
 
-//now serving on port (default 8080)
-app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`));
+// now serving on port (default 8080)
+app.listen(PORT, () => console.log(`Living large on port ${PORT}.`));
