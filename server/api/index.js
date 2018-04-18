acc70bb6852b3a724384d2be0f7f788952676131
Georgina Hoagland <georgina.hoagland@gmail.com>
2018-04-18 17:39:02 -0400

    linting

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,11 @@ const router = require('express').Router();
 
 module.exports = router;
 
-//api routes here
-router.get('/test', (req, res, next) => res.sendStatus(200))
+// api routes here
+router.get('/test', (req, res) => res.sendStatus(200));
 
-//error-handling
-router.use(function (req, res, next) {
+// error-handling
+router.use((req, res, next) => {
   const err = new Error('Not found.');
   err.status = 404;
   next(err);
